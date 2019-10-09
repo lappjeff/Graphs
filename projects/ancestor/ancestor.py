a8e8385294beaa064a5d2bca4ed899477339375d
Jeff Lapp <lappjeff20@gmail.com>
2019-10-09 12:36:08 -0400

    Finished ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -2,43 +2,64 @@
 #vertex - integer identifier for parent or child
 #each child can be a parent and each parent can be a child
 
-class Stack():
+class Queue():
     def __init__(self):
-        self.stack = []
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
         if self.size() > 0:
-            return self.stack.pop()
+            return self.queue.pop(0)
         else:
             return None
     def size(self):
-        return len(self.stack)
+        return len(self.queue)
+
+class Graph():
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    def add_edges(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
 
 def earliest_ancestor(ancestors, starting_node):
-    # child-parents pairs
-    parent_dict = {}
-    end_nodes = set()
-    for item in ancestors:
-        if not parent_dict.get(item[-1]):
-            parent_dict[item[-1]] = set([item[0]])
-        else:
-            parent_dict[item[-1]].add(item[0])
-    def recursive_helper(node, dict):
-        nonlocal end_nodes
+    # build ancestor graph
+    graph = Graph()
 
-        print(f"At node {node}")
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
 
-        if dict.get(node) is None:
-            print(f"No parents for node {node}")
-            end_nodes.add(node)
+        #build edges in reverse
+        graph.add_edges(pair[1], pair[0])
 
-        else:
-            for node in dict.get(node):
-                recursive_helper(node, dict)
+    q = Queue()
+    q.enqueue([starting_node])
 
-    recursive_helper(starting_node, parent_dict)
-    print(f"Parents dictionary: {parent_dict}")
+    max_path_length = 1
+    earliest_ancestor = -1
+
+    while q.size() > 0:
+        path = q.dequeue()
+        vertex = path[-1]
+
+        if (len(path) >= max_path_length and vertex < earliest_ancestor) or (len(path) > max_path_length):
+            earliest_ancestor = vertex
+            max_path_length = len(path)
+
+        for neighbor in graph.vertices[vertex]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+
+    return earliest_ancestor
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 print(earliest_ancestor(test_ancestors, 8))
