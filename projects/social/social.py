6e42c3d960189c7fa933daa976afe4ff0f14a947
Jeff Lapp <lappjeff20@gmail.com>
2019-10-09 19:41:03 -0400

    Finished social problem

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,24 @@
+# person - vertex
+# connection - edge
+# if person 1 is friends with person 2, person 2 is also friends with person 1 - bi-directional
+
+import random
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
 
 
 class User:
@@ -44,12 +65,32 @@ class SocialGraph:
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
-        # Add users
+        run_count = 0
 
-        # Create friendships
+        max_friends = round(numUsers / avgFriendships)
+        if numUsers < avgFriendships:
+            print("Please make sure your user count is greater than the average count")
+            return None
+        else:
+            users = []
+            # Add users
+            for number in range(numUsers):
+                self.addUser(number + 1)
+                users.append(number + 1)
+            # Create friendships
 
+            for user_id in users:
+                # add x number of friends, where x is a random num in range 0 to round(numUsers / avgFriendships)
+                for x in range(random.randrange(0, stop = avgFriendships + 1)):
+                    random.shuffle(users)
+                    # call addFriendship(number, x)
+                    if user_id < users[x]:
+                        self.addFriendship(user_id, users[x])
+                        run_count += 1
+                        # add users[x] to visited to ensure it's not revisited
+
+        print(f"************Ran addFriendship {run_count} times")
     def getAllSocialPaths(self, userID):
         """
         Takes a user's userID as an argument
@@ -59,14 +100,31 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        q = Queue()
+        visited = {}
+
+        q.enqueue([userID])
+
+        while q.size() > 0:
+            path = q.dequeue()
+            vertex = path[-1]
+            if vertex not in visited:
+                visited[vertex] = path
+
+
+            for neighbor in self.friendships[vertex]:
+                if neighbor not in visited:
+                    path_copy = list(path)
+                    path_copy.append(neighbor)
+                    q.enqueue(path_copy)
+
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
+    # sg.populateGraph(1000, 20)
     print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print(connections)
